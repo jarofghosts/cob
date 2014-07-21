72c15d4b625c80c917a78443087a87eb2af2131d
jesse keane <me@jessekeane.me>
2014-07-20 21:33:23 -0700

    add test file

---
+++ test/index.js
@@ -0,0 +1,39 @@
+var test = require('tape')
+
+var cob = require('../')
+
+test('queues pretty printed json on write', function(t) {
+  var testObj = {hello: {world: true}}
+    , cobStream = cob()
+
+  cobStream.on('data', function(data) {
+    t.equal(data, JSON.stringify(testObj, null, 2) + '\n')
+    t.end()
+  })
+
+  cobStream.end(JSON.stringify(testObj))
+})
+
+test('streams values at dotpath if specified', function(t) {
+  var testObj = {hello: {world: true}}
+    , cobStream = cob('hello.world')
+
+  cobStream.on('data', function(data) {
+    t.equal(data, JSON.stringify(true) + '\n')
+    t.end()
+  })
+
+  cobStream.end(JSON.stringify(testObj))
+})
+
+test('streams values at dotpath if specified', function(t) {
+  var testObj = {hello: {world: true}}
+    , cobStream = cob({'hello.world': false})
+
+  cobStream.on('data', function(data) {
+    t.equal(data, JSON.stringify({hello: {world: false}}, null, 2) + '\n')
+    t.end()
+  })
+
+  cobStream.end(JSON.stringify(testObj))
+})
